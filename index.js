3dead5bc0419e0b403414cf6384aaa6a2606b1cb
saktheeswaran <ssaktheeswaran25@gmail.com>
2022-11-09 13:44:58 +0530

    updated my first code

---
+++ index.js
@@ -0,0 +1,171 @@
+
+   /* let fname="sakthi";
+    let native="thanjavur";
+    let resident="palladam";
+    let degree="BSC COMPUTER SCEINCE";
+    let college="covai arts";
+    let cgpa=99.9;
+    let school="Boys higher secondry school";
+    let percentage=98;
+
+    console.log(`Myself ${fname}, i am hailing from ${native} local resident of ${resident}.
+    i have completed my ${degree} ug degree in ${college} with aggregation of ${cgpa} and
+     i did my schooling in ${school} with ${percentage} 
+                                            THANK YOU ❤❤❤`)
+
+    alert(`Myself ${fname}, i am hailing from ${native} local resident of ${resident}.
+    i have completed my ${degree} ug degree in ${college} with aggregation of ${cgpa} and
+     i did my schooling in ${school} with ${percentage} 
+                                            THANK YOU ❤❤❤`)*/
+    let person=
+    {
+        fname:"sakthi",
+        lname:"Tj",
+        age:21,
+        city:"palladam",
+
+        native:{
+            city:"thanjavur",
+            doorno:1223
+        }
+    }
+
+    let array=["element1","element2","element3"]
+
+    function vote(age){
+        if(age>18){
+            console.log("ur eligible for vote")
+        } else
+        {
+            console.log("ur not eligible vote ");
+        }
+    }
+
+   // vote(23)
+let a=10  
+let b=22
+ let c=5
+ let d=8
+ // console.log(++a+b-a+d++)
+    let x=10
+    x*=x
+  //  alert(x);
+
+     function func(age){
+
+        let check=age>18 ? "adult" : "child"
+        console.log(check);
+     }
+
+     //func(18)
+     let array1=[12,23,33,44,55,"sakthi"]
+        
+
+    // console.log(array1)
+
+    //  let hrs=new Date().getHours();
+    //  //let hrs=hour.getHours;
+    //  console.log(hrs);
+    // if(hrs>=6 && hrs <12){
+    //     console.log("Good Morning🐱‍🏍");
+    // }
+    // else if(hrs>12 && hrs<15) {
+    //         console.log("Good Aternoon😍");
+
+    // }
+    // else if(hrs=>15 && hrs<=20){
+    //     console.log("Good Evening❤");
+    // }
+    // else{
+    //     console.log("Good night😘");
+    // }
+
+
+    let amt=80000;
+    switch(true){
+        case(amt>=10000 && amt<60000):
+            console.log("Buy Android Mobile");
+        break;
+        case(amt>=60000):
+            console.log("Buy Iphone Mobile");
+        break;
+        case(amt=>5000 && amt<10000):
+            console.log("Buy Basic Mobiles");
+        break;
+        default:
+            console.log("You can't afford buy Mobile Phone");    
+
+
+    }
+    for(let i=100;i>=0;i--){
+        if(i%2==0){
+            console.log(`the even num ${i}`);
+        }
+    }
+
+    let object={
+        name:"kunjumon",
+        athu:34,
+        ithu:"therla",
+
+
+    }
+    for(let key of object){
+        console.log(key);
+    }
+    //
+
+
+
+    // for(let key in object){
+    //     console.log(`${key}:${object[key]}`);
+    // }
+
+     //
+
+
+
+       // alert("one day is our life will change like ous that day there are going to miss you more who were rejected you for sappa reason😢😢😢😢😢😢😢😢😢"); 
+    //x=x*xthis 
+    // console.log(array)
+    // console.log(array.length);
+    // array[3]=32
+
+
+
+    // console.log(person)
+    // console.log(person.fname.length)
+    // console.log(person.native)
+    // console.log(person.native.city)
+
+"sakthi is a best student our class which means his only in best "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
